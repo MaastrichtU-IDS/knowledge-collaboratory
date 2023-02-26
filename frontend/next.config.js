a5dec18227c48f5954d0ead77203fbb06c851e55
Vincent Emonet <vincent.emonet@gmail.com>
2023-02-26 11:34:30 +0100

    fix dependencies warnings and delete useless images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const { withExpo } = require('@expo/next-adapter');
-// const withImage = require('next-images')
 
 const nextConfig = withExpo({
   experimental: {
