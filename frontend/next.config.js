064df2f75701777c7ec37b4275814bd607b693bd
Vincent Emonet <vincent.emonet@gmail.com>
2023-02-26 11:05:59 +0100

    fix images

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const { withExpo } = require('@expo/next-adapter');
+const withImage = require('next-images')
 
-const nextConfig = withExpo({
+const nextConfig = withExpo(withImage({
   experimental: {
     appDir: true,
   },
@@ -9,6 +10,6 @@ const nextConfig = withExpo({
     config.experiments.topLevelAwait = true
     return config;
   },
-})
+}))
 
 module.exports = nextConfig
