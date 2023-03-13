7d80c3835ad2ede8f28a7639d324f781cd8304d8
Vincent Emonet <vincent.emonet@gmail.com>
2023-03-13 11:50:39 +0100

    improve pwa

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -14,7 +14,45 @@ export default defineConfig({
     react(),
     image(),
     // serviceWorker()
-    AstroPWA()
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['icon.png'],
+      manifest: {
+        name: 'Knowledge Collaboratory',
+        short_name: 'Knowledge Collaboratory',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/index.html',
+        // navigateFallback: '/404',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^index.html$/]
+        // navigateFallbackAllowlist: [/^\/404$/],
+      },
+    })
     // tailwind({
     //   config: {
     //     applyBaseStyles: false,
