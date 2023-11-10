b16fd1a7be436ffb383c980f8bc76c39554bff1f
Vincent Emonet <vincent.emonet@gmail.com>
2023-11-10 13:53:12 +0100

    use fcose instead of cose-bilken for cytoscape js layout

--- frontend/src/components/CytoscapeRdf.tsx
+++ frontend/src/components/CytoscapeRdf.tsx
@@ -158,7 +158,7 @@ const displayLink = (urlString: string) => {
 export function CytoscapeRdfGraph({
   rdf,
   cytoscapeElems = rdfToCytoscape(rdf),
-  layout = defaultLayout['cose-bilkent']
+  layout = defaultLayout['fcose']
 }: any) {
   const containerRef: any = useRef()
 
@@ -313,58 +313,6 @@ export function CytoscapeRdfGraph({
 // Change Cytoscape layout: https://js.cytoscape.org/#layouts
 // Layout options for dagre:
 const defaultLayout = {
-  'cose-bilkent': {
-    name: 'cose-bilkent',
-    // Called on `layoutready`
-    ready: function () {},
-    // Called on `layoutstop`
-    stop: function () {},
-    // 'draft', 'default' or 'proof"
-    // - 'draft' fast cooling rate
-    // - 'default' moderate cooling rate
-    // - "proof" slow cooling rate
-    quality: 'default',
-    // Whether to include labels in node dimensions. Useful for avoiding label overlap
-    nodeDimensionsIncludeLabels: false,
-    // number of ticks per frame; higher is faster but more jerky
-    refresh: 30,
-    // Whether to fit the network view after when done
-    fit: true,
-    // Padding on fit
-    padding: 10,
-    // Whether to enable incremental mode
-    randomize: true,
-    // Node repulsion (non overlapping) multiplier
-    nodeRepulsion: 4500,
-    // Ideal (intra-graph) edge length
-    idealEdgeLength: 200,
-    // Divisor to compute edge forces
-    edgeElasticity: 0.45,
-    // Nesting factor (multiplier) to compute ideal edge length for inter-graph edges
-    nestingFactor: 0.1,
-    // Gravity force (constant)
-    gravity: 0.25,
-    // Maximum number of iterations to perform
-    numIter: 2500,
-    // Whether to tile disconnected nodes
-    tile: true,
-    // Type of layout animation. The option set is {'during', 'end', false}
-    animate: false,
-    // Duration for animate:end
-    animationDuration: 500,
-    // Amount of vertical space to put between degree zero nodes during tiling (can also be a function)
-    tilingPaddingVertical: 10,
-    // Amount of horizontal space to put between degree zero nodes during tiling (can also be a function)
-    tilingPaddingHorizontal: 10,
-    // Gravity range (constant) for compounds
-    gravityRangeCompound: 1.5,
-    // Gravity force (constant) for compounds
-    gravityCompound: 1.0,
-    // Gravity range (constant)
-    gravityRange: 3.8,
-    // Initial cooling factor for incremental layout
-    initialEnergyOnIncremental: 0.5
-  },
   fcose: {
     name: 'fcose',
     // 'draft', 'default' or 'proof'
@@ -442,6 +390,58 @@ const defaultLayout = {
     /* layout event callbacks */
     // ready: () => {}, // on layoutready
     // stop: () => {} // on layoutstop
+  },
+  'cose-bilkent': {
+    name: 'cose-bilkent',
+    // Called on `layoutready`
+    ready: function () {},
+    // Called on `layoutstop`
+    stop: function () {},
+    // 'draft', 'default' or 'proof"
+    // - 'draft' fast cooling rate
+    // - 'default' moderate cooling rate
+    // - "proof" slow cooling rate
+    quality: 'default',
+    // Whether to include labels in node dimensions. Useful for avoiding label overlap
+    nodeDimensionsIncludeLabels: false,
+    // number of ticks per frame; higher is faster but more jerky
+    refresh: 30,
+    // Whether to fit the network view after when done
+    fit: true,
+    // Padding on fit
+    padding: 10,
+    // Whether to enable incremental mode
+    randomize: true,
+    // Node repulsion (non overlapping) multiplier
+    nodeRepulsion: 4500,
+    // Ideal (intra-graph) edge length
+    idealEdgeLength: 200,
+    // Divisor to compute edge forces
+    edgeElasticity: 0.45,
+    // Nesting factor (multiplier) to compute ideal edge length for inter-graph edges
+    nestingFactor: 0.1,
+    // Gravity force (constant)
+    gravity: 0.25,
+    // Maximum number of iterations to perform
+    numIter: 2500,
+    // Whether to tile disconnected nodes
+    tile: true,
+    // Type of layout animation. The option set is {'during', 'end', false}
+    animate: false,
+    // Duration for animate:end
+    animationDuration: 500,
+    // Amount of vertical space to put between degree zero nodes during tiling (can also be a function)
+    tilingPaddingVertical: 10,
+    // Amount of horizontal space to put between degree zero nodes during tiling (can also be a function)
+    tilingPaddingHorizontal: 10,
+    // Gravity range (constant) for compounds
+    gravityRangeCompound: 1.5,
+    // Gravity force (constant) for compounds
+    gravityCompound: 1.0,
+    // Gravity range (constant)
+    gravityRange: 3.8,
+    // Initial cooling factor for incremental layout
+    initialEnergyOnIncremental: 0.5
   },
   dagre: {
     name: 'dagre',
